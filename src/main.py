e6d95bc4c1e04326e6424189f2c17277b787e390
Erkam Boyacioglu <boyaciogluerkam@gmail.com>
2024-01-09 21:14:31 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask,redirect,url_for,render_template
 import wikipediaapi
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -26,12 +27,26 @@ def home():
    day_of_the_week = datetime.datetime.today().weekday() #monday is 0, sunday is 6
     
    todays_topic = wiki_topics[day_of_the_week]
+   headers = {
+      "User_Agent" : "WikipediaRandomGen/0.0 (https://github.com/BerkKoksal/Wikipedia_Article_Randomizer; erkanbobo33@gmail.com)"
+      }
 
-   wiki_wiki = wikipediaapi.Wikipedia("en")
-   selected_page = wiki_wiki.page(f"Portal:{todays_topic}")
    
-   print(selected_page.fullurl)
-   return render_template("Template.html", todays_website = selected_page.fullurl)
+
+   # wiki_wiki = wikipediaapi.Wikipedia("en")
+   # selected_page = wiki_wiki.page(f"Portal:{todays_topic}")
+   
+   search_url = f'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&utf8='
+   
+   response = requests.get(search_url, headers=headers)
+   data = response.json()
+   article_title = data['query']['random'][0]['title']
+   article_url = f'https://en.wikipedia.org/wiki/{article_title}'
+
+   
+
+   # response= requests.get(selected_page.fullurl, headers= headers)   
+   return render_template("Template.html", article_url = article_url)
     
 
 
