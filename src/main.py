c5d54d97cd1fcbd9effbcbb7d9d5e710ae53ea0b
BerkKoksal <151423150+BerkKoksal@users.noreply.github.com>
2024-01-09 21:22:36 -0500

    a

--- src/main.py
+++ src/main.py
@@ -1,39 +1,21 @@
 from flask import Flask,redirect,url_for,render_template
-import wikipediaapi
-import datetime
 
 app = Flask(__name__)
 
 
-# def decorator():
-#     def wrapper(func):
-#         home()
-#         print("essek")
 
 @app.route("/")
 def home():
-   
-   wiki_topics = [
-      "Math",
-      "Biology",
-      "Physics",
-      "Music",
-      "Geography",
-      "History",
-      "Technology"
-   ]
-   
-   day_of_the_week = datetime.datetime.today().weekday() #monday is 0, sunday is 6
-    
-   todays_topic = wiki_topics[day_of_the_week]
+    return render_template("Template.html")
 
-   wiki_wiki = wikipediaapi.Wikipedia("en")
-   selected_page = wiki_wiki.page(f"Portal:{todays_topic}")
-   
-   print(selected_page.fullurl)
-   return render_template("Template.html", todays_website = selected_page.fullurl)
-    
+# @app.route("/<name>/")
+# def user(name):
+#     return f"Hello {name}"
 
+
+# @app.route("/admin/")
+# def admin():
+#     return redirect((url_for("user", name = "Admin!")))
 
 if __name__ == "__main__":
     app.run()
